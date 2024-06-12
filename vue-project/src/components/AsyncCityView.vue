71f6d94d574fd6178f73b8682d733d55cdb2778b
lucjovi2001 <171295225+lucjovi2001@users.noreply.github.com>
2024-06-12 11:45:44 -0700

    Add temperatures and icons to 7 day forecast

--- vue-project/src/components/AsyncCityView.vue
+++ vue-project/src/components/AsyncCityView.vue
@@ -68,6 +68,22 @@
         <div class="max-w-screen-md w-full py-12">
             <div class="mx-8 text-white">
                 <h2 class="mb-4">7 Day Forecast</h2>
+                <div v-for="day in weatherData.daily" :key="day.dt" class="flex items-center">
+                <p class="flex-1">
+                    {{ new Date(day.dt * 1000).toLocaleDateString(
+                        "en-us",
+                        {
+                            weekday: "long",
+                        }
+                    ) 
+                    }}
+                </p>
+                <img class="w-[50px] h-[50px] object-cover" :src="`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`" alt="" />
+                <div class="flex gap-2 flex-1 justify-end">
+                    <p>H: {{ Math.round(day.temp.max) }}</p>
+                    <p>L: {{ Math.round(day.temp.min) }}</p>
+                </div>
+                </div>
             </div>
         </div>
     </div>
