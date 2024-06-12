93ca3e5e36e066209f01cd9380961eaf4ceb6e75
lucjovi2001 <171295225+lucjovi2001@users.noreply.github.com>
2024-06-12 09:14:56 -0700

    Add info icon and hover effects

--- vue-project/src/components/SiteNavigation.vue
+++ vue-project/src/components/SiteNavigation.vue
@@ -1,14 +1,20 @@
 <template>
     <header class="sticky top-0 bg-weather-primary shadow-lg">
         <nav class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6">
-            <div class="flex items-center gap-3 flex-1">
-                <i class="fa-solid fa-sun text-2xl"></i>
-                <p class="text-2xl">Local Weather</p>
+            <RouterLink :to="{ name: 'home' }">
+                <div class="flex items-center gap-3">
+                    <i class="fa-solid fa-sun text-2xl"></i>
+                    <p class="text-2xl">Local Weather</p>
+                </div>
+            </RouterLink>
+
+            <div class="flex gap-3 flex-1 justify-end">
+                <i class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
             </div>
         </nav>
     </header>
 </template>
 
-<script setup></script>
-
-<style lang="scss" scoped></style>
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
