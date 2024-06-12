ae5209648512843e7bede0aadcf995865b059155
lucjovi2001 <171295225+lucjovi2001@users.noreply.github.com>
2024-06-12 10:42:35 -0700

    Add router push functionality for search selects

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -16,7 +16,7 @@
         No results match your query. Try a different term.
       </p>
         <template v-else>
-          <li v-for="searchResult in mapboxSearchResults" :key="searchResult.id" class="py-2 cursor-pointer">
+          <li v-for="searchResult in mapboxSearchResults" :key="searchResult.id" class="py-2 cursor-pointer" @click="previewCity(searchResult)">
             {{ searchResult.place_name }}
           </li>
         </template>
@@ -28,6 +28,21 @@
 <script setup>
 import { ref } from "vue";
 import axios from "axios";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const previewCity = (searchResult) => {
+  const [city, state] = searchResult.place_name.split(",");
+  router.push({
+    name: 'cityView',
+    params: { state: state.replaceAll(" ", ""), city: city },
+    query: {
+      lat: searchResult.geometry.coordinates[1],
+      lng: searchResult.geometry.coordinates[0],
+      preview: true,
+    }
+  })
+};
 
 const mapboxAPIKey = "pk.eyJ1Ijoiam9obmtvbWFybmlja2kiLCJhIjoiY2t5NjFzODZvMHJkaDJ1bWx6OGVieGxreSJ9.IpojdT3U3NENknF6_WhR2Q";
 const searchQuery = ref("");
